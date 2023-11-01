7ab22ff51fca476ceaab1c2b47cca177ff574b1f
Yigit Aktan <51110247+yigitaktan@users.noreply.github.com>
2023-11-02 00:47:38 +0300

    Update create-demo-db.sql

--- create-demo-db.sql
+++ create-demo-db.sql
@@ -17,16 +17,19 @@
 │            │    - Inserts sample data into 'ProductCategories' and 'Products'.                                                               │
 │            │    - Using loops, populates 'Users', 'Addresses', 'Orders', and 'OrderDetails' with sample data.                                │
 │            │    - Creates several stored procedures with the names:                                                                          │
-│            │      - sp_AddUser                                                                                                               │
+│            │      - sp_AddUserAndAddress                                                                                                     │
 │            │      - sp_UpdateUser                                                                                                            │
 │            │      - sp_DeleteUser                                                                                                            │
 │            │      - sp_AddOrder                                                                                                              │
-│            │      - sp_GetOrder                                                                                                              │
+│            │      - sp_AddOrderDetail                                                                                                        │
+│            │      - sp_GetAllUsers                                                                                                           │
+│            │      - sp_GetOrdersByUser                                                                                                       │
+│            │      - sp_GetOrderDetails                                                                                                       │
 │            │      - sp_AddProduct                                                                                                            │
 │            │      - sp_UpdateProduct                                                                                                         │
 │            │      - sp_DeleteProduct                                                                                                         │
-│            │      - sp_GetProduct                                                                                                            │
-│            │      - sp_GetCategory                                                                                                           │
+│            │      - sp_GetProductsByCategory                                                                                                 │
+│            │      - sp_GetProductCategories                                                                                                  │
 │            │ 4. Switches to the 'master' database and manages a server login:                                                                │
 │            │    - Checks if a server login named 'MyDemoUser' exists. If it does, it drops it.                                               │
 │            │    - Creates the 'MyDemoUser' login with a specified password.                                                                  │
